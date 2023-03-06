e4d0320d2b1a7a7fa79135a4ba4e6b67a38df446
Cm <109184310+cmspeedrunner@users.noreply.github.com>
2023-03-06 11:43:26 +0000

    Setup file, run this b4 anything else

---
+++ setup.py
@@ -0,0 +1,6 @@
+import os
+os.startfile("C:/Castro/resources/setup.bat", "runas")
+print("\033[1;34m<<setup.bat>>")
+print("--------------------")
+print("\033[1;32mInstalled sucessfully!")
+print("Purpose of setup.bat is to link the icon file with any .cst file and to give the file description of .cst files to 'Castro Source File'\033[0m")
